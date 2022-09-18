19464937e8436e9865402b7d315572f57384f082
Felix Orinda <forinda82@gmail.com>
2022-09-18 06:14:55 +0300

    Test fix

--- test/blah.test.tsx
+++ test/blah.test.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Thing } from '../src';
+import { LoaderComponent } from '../src';
 
 describe('it', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Thing />, div);
+    ReactDOM.render(<LoaderComponent loading />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 });
