d7ae50f5c2c4200fbeea7ae0e1805325d9be6ab9
Felix Orinda <forinda82@gmail.com>
2022-09-18 06:12:15 +0300

    Lint fix

--- src/index.tsx
+++ src/index.tsx
@@ -3,23 +3,26 @@ import * as React from 'react';
 
 interface LoaderProps {
   loading: boolean;
-  loadingColor?: string
+  loadingColor?: string;
   backgroundColor?: string;
 }
 
-export const LoaderComponent = ({ loading, backgroundColor = 'rgba(236, 240, 241, 0.7)', loadingColor = '#e74c3c' }: LoaderProps) => {
-
+export const LoaderComponent = ({
+  loading,
+  backgroundColor = 'rgba(236, 240, 241, 0.7)',
+  loadingColor = '#e74c3c',
+}: LoaderProps) => {
   const backgroundColorStyle = backgroundColor ? { backgroundColor } : {};
-  const loadingColorStyle = loadingColor ? { backgroundColor: loadingColor } : {};
+  const loadingColorStyle = loadingColor
+    ? { backgroundColor: loadingColor }
+    : {};
 
-  return (
-
-    loading ? <div className='loading_background' style={backgroundColorStyle}>
-      <div className='loading_bar' >
-        <div className='loading_circle_1' style={loadingColorStyle} />
-        <div className='loading_circle_2' style={loadingColorStyle} />
+  return loading ? (
+    <div className="loading_background" style={backgroundColorStyle}>
+      <div className="loading_bar">
+        <div className="loading_circle_1" style={loadingColorStyle} />
+        <div className="loading_circle_2" style={loadingColorStyle} />
       </div>
-    </div> : null
-
-  );
+    </div>
+  ) : null;
 };
